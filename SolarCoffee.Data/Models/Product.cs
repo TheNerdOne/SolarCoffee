2e08dda89a81c31998ddabad0fc69705867e96d4
poooriiich <poooriiich@icloud.com>
2025-05-05 00:15:23 +0330

    update dto & add response model

--- SolarCoffee.Data/Models/Product.cs
+++ SolarCoffee.Data/Models/Product.cs
@@ -10,12 +10,19 @@ namespace SolarCoffee.Data.Models
         public DateTime UpdatedOn { get; set; }
 
         [MaxLength(64)]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [MaxLength(128)]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public decimal Price { get; set; }
         public bool IsTaxable { get; set; }
         public bool IsArchived { get; set; }
     }
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+        public required string Message { get; set; }
+    }
 }
+
+
