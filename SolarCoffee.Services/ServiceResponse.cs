b8d01a51771dc6cbfcad9b894a3bc4686ce2ec68
poooriiich <poooriiich@icloud.com>
2025-05-05 00:15:32 +0330

    update dto

--- SolarCoffee.Services/ServiceResponse.cs
+++ SolarCoffee.Services/ServiceResponse.cs
@@ -7,8 +7,8 @@ namespace SolarCoffee.Services
     public class ServiceResponse<T>
     {
         public bool IsSuccess { get; set; }
-        public string Message { get; set; }
+        public required string Message { get; set; }
         public DateTime Time { get; set; }
-        public T Data { get; set; }
+        public required T Data { get; set; }
     }
 }
