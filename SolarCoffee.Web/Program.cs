7a8e9f373d47f34ae1b4e4962c4e4a54248b31d2
poooriiich <poooriiich@icloud.com>
2025-05-05 00:16:16 +0330

    update swagger config

--- SolarCoffee.Web/Program.cs
+++ SolarCoffee.Web/Program.cs
@@ -1,7 +1,20 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+builder.Services.AddDbContext<SolarDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("solar.dev")));
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
