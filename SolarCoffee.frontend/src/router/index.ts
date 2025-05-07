8c690fc50ec9f1af15b3c19b1310683f742dab53
poooriiich <poooriiich@icloud.com>
2025-05-07 23:41:09 +0330

    update router

--- SolarCoffee.frontend/src/router/index.ts
+++ SolarCoffee.frontend/src/router/index.ts
@@ -16,6 +16,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductsView.vue')
+    },
+    {
+      path: '/product/:productId',
+      name: 'product',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductView.vue')
     }
   ]
 })
