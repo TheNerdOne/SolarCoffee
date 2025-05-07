6fb101e6332b3b54feef11c9e92899dc7ca1c0f2
poooriiich <poooriiich@icloud.com>
2025-05-07 23:41:05 +0330

    add api config

---
+++ SolarCoffee.frontend/src/services/api.ts
@@ -0,0 +1,28 @@
+import axios, { type Method, type AxiosResponse } from 'axios'
+import type { AxiosError } from 'axios'
+import { BASE_URL } from '../../config/services'
+
+export async function makeRequest<T>(
+    method: Method,
+    endpoint: string,
+    data?: unknown
+): Promise<T> {
+    try {
+        const response: AxiosResponse<T> = await axios({
+            method,
+            url: `${BASE_URL}${endpoint}`,
+            data,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        return response.data.data
+    } catch (error: AxiosError | unknown) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(error.response?.data?.message || error.message)
+        }
+        throw new Error('An unexpected error occurred')
+    }
+}
