46571b6af2fee3b4d9907adad523e647d7c24597
poooriiich <poooriiich@icloud.com>
2025-05-07 20:23:01 +0330

    db generated id for products

--- SolarCoffee.Data/Models/Product.cs
+++ SolarCoffee.Data/Models/Product.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SolarCoffee.Data.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
